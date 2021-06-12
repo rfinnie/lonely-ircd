ec2095fd4fb5498fa64c69b3c6727435cf79ad5f
Ryan Finnie <ryan@finnie.org>
2021-06-11 21:29:36 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name="lonely-ircd",
+    description="The loneliness IRC server",
+    license="MPL-2.0",
+    author="Ryan Finnie",
+    author_email="ryan@finnie.org",
+    scripts=["lonely-ircd"],
+)
